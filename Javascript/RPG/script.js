a5f77d68195e45eeb7feecc2d6b28264bbf5b796
UchooseCode <uchoosemarketingonline@gmail.com>
2025-10-10 18:46:55 -0400

    temporary removed let and const to test on browser

--- Javascript/RPG/script.js
+++ Javascript/RPG/script.js
@@ -1,21 +1,21 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeapon = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
+xp = 0;
+ health = 100;
+ gold = 50;
+ currentWeapon = 0;
+ fighting = 0;
+ monsterHealth = 0;
+ inventory = ["stick"];
 
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
-const goldText = document.querySelector("#goldText");
-const monsterStats = document.querySelector("#monsterStats");
-const monsterName = document.querySelector("#monsterName");
-const monsterHealthText = document.querySelector("#monsterHealthText");
+ button1 = document.querySelector("#button1");
+ button2 = document.querySelector("#button2");
+ button3 = document.querySelector("#button3");
+ text = document.querySelector("#text");
+ xpText = document.querySelector("#xpText");
+ healthText = document.querySelector("#healthText");
+ goldText = document.querySelector("#goldText");
+ monsterStats = document.querySelector("#monsterStats");
+ monsterName = document.querySelector("#monsterName");
+ monsterHealthText = document.querySelector("#monsterHealthText");
 
 
 //initialize buttons
